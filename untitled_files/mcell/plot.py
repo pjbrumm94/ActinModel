2fd63f88ea62464cf70e24c05c8726cf20966ec1
pjbrumm94 <pjbumm@gmail.com>
2018-11-30 16:49:17 -0500

    Molecular Count Plotting Function

--- untitled_files/mcell/plot.py
+++ untitled_files/mcell/plot.py
@@ -1,14 +1,29 @@
-import pylab, os, glob
+# PURPOSE: This code is used for plotting the number of molecules over the 
+#entire simulation time. Data held in ".dat" files in the react_data folder
+# AUTHOR: Peter J Brumm
+# DATE: 11/30/2018
 
-path = "./react_data/"
-x = os.listdir(path)
-print(x)
-#s = [x for x in os.listdir(path) if x.endswith('.')]
-#print(s)
+import pylab, os
 
-#s = glob.glob("*.dat")
-#print(s)
-#for species in os.listdir('*.dat'):
-#	print(species)
+# Create path to .dat files and retainer list
+path	= "./react_data/"
+flist	= []
 
-#datalist = [( pylab.loadtxt(filename), label ) for filename, label in s)
+# Show list of .dat files with labels in retainer
+for f in os.listdir(path):
+	if f.endswith('.dat'):
+		flist.append((f, f.split(".")[0]))
+
+# Import timecourse data and associate with label
+datalist = [( label, pylab.loadtxt(path+filename))
+		for filename, label in flist]
+
+# For each data file, plot timecourse data with label
+for label, data in datalist:
+	pylab.plot(data[:,0], data[:,1], label=label)
+
+# PyLab Plotting Area
+pylab.legend()
+pylab.xlabel('Timestep')
+pylab.ylabel('Molecule Count')
+pylab.show()
